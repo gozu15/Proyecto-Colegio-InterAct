7a2d7868cbd7004a392f6cc7e3e0bc20f5390f55
ejqv <ejqv1996.18@gmail.com>
2018-12-17 11:57:12 -0400

    Borrando Archivos

--- src/Controllers/ParaleloController.java
+++ src/Controllers/ParaleloController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -50,37 +50,3 @@ public class ParaleloController {
         return response;
     }
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Controllers;
-
-import DataBaseManager.ParaleloDataBase;
-import Models.Paralelo;
-import java.util.List;
-
-/**
- *
- * @author Wilver Hidalgo Barja
- */
-public class ParaleloController {
-   ParaleloDataBase DbParalelo=new ParaleloDataBase();
-    public int Create(Paralelo paralelo){
-        
-        int response=1;
-        if(DbParalelo.Insert(paralelo)==1){
-            
-        }else{
-            response=0;
-        }
-        return response;
-    }
-    public List<Paralelo> Select(){
-        return DbParalelo.Select();
-    }   
-    
-}
->>>>>>> 889cc8ae75088e16bef307684967fe0f6980d24e
